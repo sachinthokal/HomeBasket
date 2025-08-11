5116384d699a4b54f1a1527dec3992589c5b5ee0
sachinthokal <sachinthokal1799@gmail.com>
2025-08-12 02:15:07 +0530

    Site Name Updated

--- homebasket_frontend/src/app/pages/category/category.ts
+++ homebasket_frontend/src/app/pages/category/category.ts
@@ -3,22 +3,23 @@ import { Item } from '../../model/item.model';
 import { CategoryService } from './category.service';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule ,RouterModule],
   templateUrl: './category.html',
   styleUrl: './category.css'
 })
 export class Category implements OnInit {
-
-  constructor(private myService: CategoryService) {
-    // You can use myService here if needed
-  }
-
+  
+  currentDateTime: string = '';
+  sidebarOpen = false;
   itemList: Item[] = [];
-
   items: Item[] = [];
+
+  constructor(private myService: CategoryService) {}
+
 
   ngOnInit(): void {
 
@@ -29,6 +30,10 @@ export class Category implements OnInit {
     
     this.myService.getAllItems().subscribe((next)=> {this.itemList = next})
   }
+  
+   toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen;
+    }
 
   onEdit(items: Item) {
     console.log('Edit clicked:', items);
--- homebasket_frontend/src/app/pages/dashboard/dashboard.ts
+++ homebasket_frontend/src/app/pages/dashboard/dashboard.ts
@@ -3,11 +3,12 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { CommonModule } from '@angular/common';
 import { Item } from '../../model/item.model';
 import { ItemService } from './item.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
