e2b8f960263111fb8b3deafde9faa183f244b19e
sachinthokal <sachinthokal1799@gmail.com>
2025-09-05 01:27:16 +0530

    URL Fix

--- homebasket-frontend/src/environments/environment.prod.ts
+++ homebasket-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-// prod config
 export const environment = {
   production: true,
-  apiUrl: 'http://homebasket_backend:8000',
+  apiUrl: 'http://homebasket_backend:8000/',
   appName: 'HomeBasket',
   debug: false
 };
