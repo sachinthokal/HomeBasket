1d5d7043d01a157683dea59e9e49be8994347cac
sachinthokal <sachinthokal1799@gmail.com>
2025-09-05 01:04:27 +0530

    Docker Updated

--- homebasket-frontend/src/environments/environment.prod.ts
+++ homebasket-frontend/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 // prod config
-// src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://api.homebasket.com/api/v1',
+  apiUrl: 'http://homebasket_backend:8000',
   appName: 'HomeBasket',
   debug: false
 };
