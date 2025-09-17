d9a711d22f917f257b17d1676aff95cdf81541f8
sachinthokal <sachinthokal1799@gmail.com>
2025-09-17 15:43:36 +0530

    Update

--- homebasket_backend/homebasket/settings.py
+++ homebasket_backend/homebasket/settings.py
@@ -103,19 +103,15 @@ WSGI_APPLICATION = 'homebasket.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB', 'homebasket'),
-#         'USER': os.environ.get('POSTGRES_USER', 'sachin'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'admin'),
-#         'HOST': os.environ.get('POSTGRES_HOST', 'db'),  # db = docker-compose service name
-#         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-#     }
-# }
-
 DATABASES = {
-     "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", 5432),
+    }
 }
 
 # # Password validation
