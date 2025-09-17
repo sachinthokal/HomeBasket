b6f49d46a0be44101e783e577832b899bd1660a3
sachinthokal <sachinthokal1799@gmail.com>
2025-09-17 16:48:48 +0530

    Backend URL Updated

--- homebasket-frontend/src/environments/environment.ts
+++ homebasket-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8000/'   // frontend la backend URL /api/ path var call karaycha
-  apiUrl: 'https://homebasket-backend.up.railway.app/'
+  apiUrl: '/api/'
 };
