ee80c04ee651dd166205754b4e8b93f3e5719fbb
Sachin Thokal <sachinthokal1799@gmail.com>
2025-09-26 19:40:58 +0530

    Update environment.ts

--- homebasket-frontend/src/environments/environment.ts
+++ homebasket-frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/'
+  // production: false,
+  // apiUrl: 'http://localhost:8000/'
+  production: true,
+  apiUrl: 'https://homebasket-backend.up.railway.app/'
 };
